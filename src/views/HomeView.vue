591b82e9e31d4d4587d221b360baaf05d144a453
GuztaJF-DS <noobshippuden13@gmail.com>
2023-04-26 11:22:19 -0300

    feat: First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const items = ref([{ message: 'aspear' }, { message: 'Bar' }]);
+const state = ref({ count: false })
+
+function reverse(randomData:any) {
+  console.log(randomData)
+  state.value.count=!state.value.count
+}
+</script>
+
+<template>
+  <div class="land">
+    <img draggable="false" src="../assets/dirt.svg">
+    <div class="berryBox">
+      <div class="berry" @click="reverse(item)" v-for="item in items" :key="item.message">
+        <img draggable="false" src="../assets/aspear.svg">
+        {{ state.count }}
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .land{
+    align-self: center;
+  }
+
+  .berryBox{
+    position: absolute;
+    left: 17px;
+    bottom:20px;
+    display: grid;
+    grid-template-columns: 48px 48px;
+    grid-column-gap: 10px;
+    grid-auto-rows: 48px;
+  }
+
+  .berry > div {
+    bottom:40px;
+  }
+
+  .berry > img {
+    bottom:40px
+  }
+  .berry:hover {
+    background-color: red;
+  }
+</style>
